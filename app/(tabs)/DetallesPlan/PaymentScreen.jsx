10920821f576e612855ccbc1839fc3a0512f6263
Lagos0 <fabriziofd2017@gmail.com>
2024-09-20 23:29:49 -0500

    PP

--- app/(tabs)/DetallesPlan/PaymentScreen.jsx
+++ app/(tabs)/DetallesPlan/PaymentScreen.jsx
@@ -1,37 +1,243 @@
-import React from 'react';
-import { View, Text, StyleSheet, ScrollView, TextInput,TouchableOpacity } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { SafeAreaView, Modal, Button, Image, View, Text, TextInput, TouchableOpacity, Alert, ScrollView, KeyboardAvoidingView, Platform, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { SafeAreaView } from 'react-native';
+import visa from '../../../assets/visa.png';
+import master from '../../../assets/master.png';
+import americanex from '../../../assets/americanex.png';
+import dinner from '../../../assets/dinner.png';
+import yape from '../../../assets/yape.png';
 
-const PaymentScreen = ({navigation}) => {
+const PaymentScreen = ({ navigation }) => {
+  const [cardNumber, setCardNumber] = useState('');
+  const [expiryDate, setExpiryDate] = useState('');
+  const [cvv, setCvv] = useState('');
+  const [dniCard, setDniCard] = useState('');
+  const [yapePhone, setYapePhone] = useState('');
+  const [dniYape, setDniYape] = useState('');
+  const [yapeConfirmation, setYapeConfirmation] = useState('');
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
+
+  const cardNumberRef = useRef(null);
+  const expiryDateRef = useRef(null);
+  const cvvRef = useRef(null);
+  const dniCardRef = useRef(null);
+  const yapePhoneRef = useRef(null);
+  const dniYapeRef = useRef(null);
+  const yapeConfirmationRef = useRef(null);
+
+  const isCardInputActive = cardNumber || expiryDate || cvv || dniCard;
+  const isYapeInputActive = yapePhone || dniYape || yapeConfirmation;
+
+  const handleExpiryDateChange = (text) => {
+    let formattedText = text.replace(/\D/g, '');
+    if (formattedText.length > 2) {
+      formattedText = `${formattedText.slice(0, 2)}/${formattedText.slice(2, 6)}`;
+    }
+    if (parseInt(formattedText.slice(0, 2), 10) > 12) {
+      formattedText = '12';
+    }
+    setExpiryDate(formattedText);
+    if (formattedText.length === 7) {
+      cvvRef.current.focus();
+    }
+  };
+
+  const handleCardNumberChange = (text) => {
+    setCardNumber(text);
+    if (text.length === 16) {
+      expiryDateRef.current.focus();
+    }
+  };
+
+  const handleCvvChange = (text) => {
+    setCvv(text);
+    if (text.length === 3) {
+      dniCardRef.current.focus();
+    }
+  };
+
+  const handleYapePhoneChange = (text) => {
+    setYapePhone(text);
+    if (text.length === 9) {
+      dniYapeRef.current.focus();
+    }
+  };
+
+  const handleYapeDniChange = (text) => {
+    setDniYape(text);
+    if (text.length === 8) {
+      yapeConfirmationRef.current.focus();
+    }
+  };
+
+  const validateFields = () => {
+    if (cardNumber && cardNumber.length !== 16) {
+      setModalMessage('El número de tarjeta debe tener 16 dígitos.');
+      setModalVisible(true);
+      
+      return false;
+    }
+    if (cardNumber && (!expiryDate || !cvv || !dniCard)) {
+      setModalMessage('Completa todos los campos de tarjeta de crédito/débito.');
+      setModalVisible(true);
+      return false;
+    }
+    if (yapePhone && (!dniYape || !yapeConfirmation)) {
+      setModalMessage('Completa todos los campos de Yape');
+      setModalVisible(true);
+      return false;
+    }
+    if (!cardNumber && !yapePhone) {
+      setModalMessage('Debes completar al menos una opción de pago.');
+      setModalVisible(true);
+
+      return false;
+    }
+    return true;
+  };
+
+  const handlePay = () => {
+    if (validateFields()) {
+      
+      setModalMessage('Tu pago ha sido procesado exitosamente');
+      setModalVisible(true);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.barraSuperior}>
-        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.botonRetroceso}>
-          <Ionicons name="arrow-back" size={24} color="white" />
-        </TouchableOpacity>
-        <Text style={styles.textoBarra}>Pago</Text>
-      </View>
-      <Text style={styles.subtitle}>Elije una forma de pago:</Text>
-      <Text style={styles.sectionTitle}>Tarjeta de Crédito o Débito</Text>
-      <View style={styles.cardContainer}>
-      <TextInput style={styles.input} placeholder="Numero de Tarjeta" keyboardType="number-pad" />
-      <View style={styles.inlineInputs}>
-        <TextInput style={[styles.input, styles.smallInput]} placeholder="--/--" keyboardType="number-pad" />
-        <TextInput style={[styles.input, styles.smallInput, styles.cvv]} placeholder="CVV" keyboardType="number-pad" />
-      </View>
-      <TextInput style={styles.input} placeholder="DNI" keyboardType="number-pad" />
-      </View>
-      <Text style={[styles.sectionTitle ,styles.yape ]}>Yape</Text>
-      <View style={styles.cardContainer}>
-      <TextInput style={styles.input} placeholder="Numero de telefono" keyboardType="number-pad" />
-      <TextInput style={styles.input} placeholder="DNI" keyboardType="number-pad" />
-      <TextInput style={styles.input} placeholder="Numero de confirmación" keyboardType="number-pad" />
-    </View>
-    <TouchableOpacity style={styles.button}>
-      <Text style={styles.buttonText}>Pagar</Text>
-    </TouchableOpacity>
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{ flex: 1 }}
+      >
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+          <View style={styles.barraSuperior}>
+            <TouchableOpacity onPress={() => navigation.goBack()} style={styles.botonRetroceso}>
+              <Ionicons name="arrow-back" size={24} color="white" />
+            </TouchableOpacity>
+            <Text style={styles.textoBarra}>Pago</Text>
+          </View>
+
+          <Text style={styles.subtitle}>Elige una forma de pago:</Text>
+
+          {/* Sección de Tarjeta de Crédito o Débito */}
+          <Text style={styles.sectionTitle}>Tarjeta de Crédito o Débito</Text>
+          <View style={styles.cardImagesContainer}>
+            <Image source={visa} style={styles.cardImage} />
+            <Image source={master} style={styles.cardImage} />
+            <Image source={americanex} style={styles.cardImage} />
+            <Image source={dinner} style={styles.cardImage} />
+          </View>
+          <View style={styles.cardContainer}>
+            <TextInput
+              ref={cardNumberRef}
+              style={styles.input}
+              placeholder="Número de Tarjeta"
+              keyboardType="number-pad"
+              value={cardNumber}
+              onChangeText={handleCardNumberChange}
+              maxLength={16}
+              editable={!isYapeInputActive}
+              returnKeyType="next"
+              onSubmitEditing={() => expiryDateRef.current.focus()}
+            />
+            <View style={styles.inlineInputs}>
+              <TextInput
+                ref={expiryDateRef}
+                style={[styles.input, styles.smallInput]}
+                placeholder="MM/AAAA"
+                keyboardType="number-pad"
+                value={expiryDate}
+                onChangeText={handleExpiryDateChange}
+                editable={!isYapeInputActive}
+                maxLength={7}
+              />
+              <TextInput
+                ref={cvvRef}
+                style={[styles.input, styles.smallInput, styles.cvv]}
+                placeholder="CVV"
+                keyboardType="number-pad"
+                value={cvv}
+                onChangeText={handleCvvChange}
+                editable={!isYapeInputActive}
+                maxLength={3}
+              />
+            </View>
+            <TextInput
+              ref={dniCardRef}
+              style={styles.input}
+              placeholder="DNI"
+              keyboardType="number-pad"
+              value={dniCard}
+              onChangeText={setDniCard}
+              editable={!isYapeInputActive}
+              maxLength={8}
+            />
+          </View>
+
+          {/* Sección de Yape */}
+          <View style={styles.yapeContainer}>
+            <Text style={styles.yapeText}>Yape</Text>
+            <Image source={yape} style={styles.yapeImage} />
+          </View>
+          <View style={styles.cardContainer}>
+            <TextInput
+              ref={yapePhoneRef}
+              style={styles.input}
+              placeholder="Número de teléfono"
+              keyboardType="number-pad"
+              value={yapePhone}
+              onChangeText={handleYapePhoneChange}
+              editable={!isCardInputActive}
+              maxLength={9}
+              returnKeyType="next"
+              onSubmitEditing={() => dniYapeRef.current.focus()}
+            />
+            <TextInput
+              ref={dniYapeRef}
+              style={styles.input}
+              placeholder="DNI"
+              keyboardType="number-pad"
+              value={dniYape}
+              onChangeText={handleYapeDniChange}
+              editable={!isCardInputActive}
+              maxLength={8}
+            />
+            <TextInput
+              ref={yapeConfirmationRef}
+              style={styles.input}
+              placeholder="Número de confirmación"
+              keyboardType="number-pad"
+              value={yapeConfirmation}
+              onChangeText={setYapeConfirmation}
+              editable={!isCardInputActive}
+              maxLength={6}
+            />
+          </View>
+
+          <TouchableOpacity style={styles.button} onPress={handlePay}>
+            <Text style={styles.buttonText}>Pagar</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <Modal
+        transparent={true}
+        animationType="slide"
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>{modalMessage}</Text>
+            <Button title="Cerrar" onPress={() => setModalVisible(false)} />
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
+
+    
   );
 };
 
@@ -44,9 +250,8 @@ const styles = StyleSheet.create({
   headerpago: {
     backgroundColor: '#4CAF50',
     padding: 16,
-    
     alignItems: 'center',
-    marginTop:35,
+    marginTop: 35,
   },
   headerTextpago: {
     color: '#fff',
@@ -56,21 +261,47 @@ const styles = StyleSheet.create({
   subtitle: {
     fontSize: 16,
     marginBottom: 8,
-    marginTop:20,
-    marginLeft:15,
+    marginTop: 20,
+    marginLeft: 15,
+  },
+  cardImagesContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  cardImage: {
+    width: 50,
+    height: 30,
+    resizeMode: 'contain',
+    marginRight: 10,
   },
   sectionTitle: {
     fontSize: 18,
     fontWeight: 'bold',
     marginTop: 20,
-    marginLeft:15,
-    marginRight:15,
+    marginLeft: 15,
   },
-  yape:{
-    marginTop:50,
+  yapeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20, 
+    marginBottom: 20, 
+    marginLeft: 15,
+  },
+  yapeText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginRight: 10,
+  },
+  yapeImage: {
+    width: 50,
+    height: 30,
+    resizeMode: 'contain',
   },
   cardContainer: {
-    marginTop: 30,
+    marginTop: 10,
   },
   input: {
     height: 40,
@@ -79,41 +310,27 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 10,
-    marginLeft:15,
-    marginRight:15,
+    marginLeft: 15,
+    marginRight: 15,
   },
   inlineInputs: {
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   smallInput: {
     width: '45%',
-    
-    
   },
-  cvv:{
-    marginLeft:-9,
-  },cardContainer: {
-    marginTop: 30,
-  },
-  input: {
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginBottom: 10,
-    marginLeft:15,
-    marginRight:15,
+  cvv: {
+    marginLeft: -9,
   },
   button: {
     backgroundColor: '#000',
     padding: 15,
     borderRadius: 5,
     alignItems: 'center',
-    marginTop: 50,
-    marginLeft:100,
-    marginRight:100,
+    marginTop: 30,
+    marginLeft: 100,
+    marginRight: 100,
   },
   buttonText: {
     color: '#fff',
@@ -126,7 +343,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     paddingHorizontal: 10,
-
   },
   botonRetroceso: {
     marginRight: 30,
@@ -136,6 +352,33 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+  modalContent: {
+    width: '80%',
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    alignItems: 'center'
+  },
+  modalText: {
+    fontSize: 16,
+    marginBottom: 15,
+    textAlign: 'center'
+  },
+  modalButton: {
+    padding: 10,
+    backgroundColor: '#007BFF',
+    borderRadius: 5,
+  },
+  modalButtonText: {
+    color: 'white',
+    fontSize: 16,
+  }
 });
 
 export default PaymentScreen;
