c4a3dd8428015d509d21b365f0f0773ec816d073
Ghost5x9 <danieltaype68@outlook.com>
2024-10-03 12:33:29 -0500

    Incorporacion de tipo de usuario en registro

--- app/(tabs)/Register/Register_II.jsx
+++ app/(tabs)/Register/Register_II.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, SafeAreaView, Modal, Button } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import styles from './Style_RegisterII';
 import { useRoute } from '@react-navigation/native';
 
@@ -7,24 +8,37 @@ const RegisterII = ({ navigation }) => {
   const route = useRoute();
   
   // Recibimos los datos de la primera pantalla RegisterI
-  const {
-    nombre,
-    apellido,
-    DNI,
-    direccion,
-    genero,
-    numero_contacto,
-  } = route.params;
+  const { nombre, apellido, DNI, direccion, genero, numero_contacto } = route.params;
 
+  const [tipoUsuario, setTipoUsuario] = useState('');
+  const [tiposUsuario, setTiposUsuario] = useState([]);
   const [correo, setCorreo] = useState('');
   const [contrasena, setContrasena] = useState('');
   const [ConfirContra, setConfirContra] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
   const [modalMessage, setModalMessage] = useState('');
 
+  // Función para obtener los tipos de usuario del backend
+  useEffect(() => {
+    const fetchTiposUsuario = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/back/tipos-usuario'); // URL para obtener los tipos de usuario
+        const data = await response.json();
+        setTiposUsuario(data);
+      } catch (error) {
+        console.error('Error al obtener los tipos de usuario:', error);
+        setModalMessage('Error al cargar los tipos de usuario.');
+        setModalVisible(true);
+        setLoading(false);
+      }
+    };
+
+    fetchTiposUsuario();
+  }, []);
+
   // Función para manejar el registro del usuario
   const handleSubmit = async () => {
-    if (!correo || !contrasena || !ConfirContra) {
+    if (!correo || !contrasena || !ConfirContra || !tipoUsuario) {
       setModalMessage('Por favor, complete todos los campos.');
       setModalVisible(true);
     } else if (contrasena !== ConfirContra) {
@@ -47,6 +61,7 @@ const RegisterII = ({ navigation }) => {
             numero_contacto,
             email: correo,
             contrasena,
+            tipo_usuario: tipoUsuario, // Enviar el tipo de usuario seleccionado
           }),
         });
 
@@ -100,6 +115,19 @@ const RegisterII = ({ navigation }) => {
         value={ConfirContra}
         onChangeText={setConfirContra}
       />
+
+      <Text style={styles.subTitle}>Por favor, escoja su tipo de usuario</Text>
+
+      <Picker
+          selectedValue={tipoUsuario}
+          onValueChange={(itemValue) => setTipoUsuario(itemValue)}
+          style={styles.picker}
+      >
+      <Picker.Item label="Seleccione el tipo de usuario"/>
+        {tiposUsuario.map((tipo) => (
+          <Picker.Item key={tipo.id} label={tipo.tipo} value={tipo.id} />
+        ))}
+      </Picker>
 
       {/* Botón para completar el registro */}
       <TouchableOpacity style={styles.button} onPress={handleSubmit}>
--- app/(tabs)/Register/Style_RegisterII.jsx
+++ app/(tabs)/Register/Style_RegisterII.jsx
@@ -1,80 +1,90 @@
 import { StyleSheet } from 'react-native';
 
 const Style_RegisterII = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'white',
-      paddingVertical: 40
-    },
-    header: {
-      height: 60,
-      backgroundColor: '#34A853',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginBottom: 30, // Aumenta el espacio entre la barra y las cajas de texto
-    },
-    headerText: {
-      color: 'white',
-      fontSize: 20,
-      fontWeight: 'semibold',
-      position: 'absolute',
-    },
-    subTitle: {
-        fontSize: 16,
-        marginBottom: 20,
-        color: '#333',
-        textAlign: 'center',
-      },
-    input: {
-        width: '90%',
-        alignSelf: 'center',
-        height: 45,
-        borderColor: '#ccc',
-        borderWidth: 1,
-        borderRadius: 10,
-        paddingHorizontal: 15,
-        marginBottom: 20,
-      },
-    button: {
-        backgroundColor: '#000',
-        paddingVertical: 15,
-        borderRadius: 10,
-        alignItems: 'center',
-        marginTop: 20,
-        width: '90%',
-        alignSelf: 'center',
-      },
-      buttonText: {
-        color: '#fff',
-        fontSize: 16,
-        fontWeight: 'bold',
-      },
-      modalOverlay: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0,0,0,0.5)'
-      },
-      modalContent: {
-        width: '80%',
-        padding: 20,
-        backgroundColor: 'white',
-        borderRadius: 10,
-        alignItems: 'center'
-      },
-      modalText: {
-        fontSize: 16,
-        marginBottom: 15
-      },
-      modalButton: {
-        padding: 10,
-        backgroundColor: '#007BFF',
-        borderRadius: 5,
-      },
-      modalButtonText: {
-        color: 'white',
-        fontSize: 16,
-      }
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingVertical: 40,
+  },
+  header: {
+    height: 60,
+    backgroundColor: '#34A853',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'semibold',
+    position: 'absolute',
+  },
+  subTitle: {
+    fontSize: 16,
+    marginBottom: 20,
+    color: '#333',
+    textAlign: 'center',
+  },
+  input: {
+    width: '90%',
+    alignSelf: 'center',
+    height: 45,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    marginBottom: 20,
+  },
+  picker: {
+    width: '90%',
+    alignSelf: 'center',
+    height: 45,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#000',
+    paddingVertical: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 20,
+    width: '90%',
+    alignSelf: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  modalContent: {
+    width: '80%',
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalText: {
+    fontSize: 16,
+    marginBottom: 15,
+  },
+  modalButton: {
+    padding: 10,
+    backgroundColor: '#007BFF',
+    borderRadius: 5,
+  },
+  modalButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
 });
 
 export default Style_RegisterII;
